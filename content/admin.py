d4e2b1a2acdab2dd54ab97515d8371f598548029
kartik <detkartik@users.noreply.github.com>
2020-09-07 22:07:11 +0530

    Update admin.py

--- content/admin.py
+++ content/admin.py
@@ -3,8 +3,6 @@ from .models import Content,Category
 # Register your models here.
 @admin.register(Content)
 class ContentAdmin(admin.ModelAdmin):
-    # fields = ['title', 'description', 'price',
-    #           'published', 'is_published', 'cover']
     list_display = ['title', 'body', 'summary', 'document']
     list_filter = ['title', 'document', 'category']
     search_fields = ['title']
